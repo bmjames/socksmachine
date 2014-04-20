150710c0579fe4a773e1641e37f80f5be5484166
Ben James <bmjames@gmail.com>
2014-04-20 10:21:45 +0100

    Playing with lifed-async

--- Main.hs
+++ Main.hs
@@ -5,14 +5,14 @@ import Prelude hiding (mapM_)
 import SocksMachine.Server
 import SocksMachine.Monad
 
-import Control.Concurrent          (forkIO)
-import Control.Monad.Trans.Control (liftBaseDiscard)
+import Control.Concurrent.Async.Lifted (concurrently)
+import Control.Monad                   (void)
 
 
 -- | Start a server, read lines from stdin and broadcast to all clients.
 main :: IO ()
 main = do
   st <- initState
-  runServ' st $ do
-    liftBaseDiscard forkIO $ serverMain "0.0.0.0" 9160
-    serverConsoleMain
+  runServ' st . void $ concurrently
+   (serverMain "0.0.0.0" 9160)
+   serverConsoleMain
